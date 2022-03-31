390734988456aa152100fc434d86b39467d40e06
GiulianoCretella <giulianocretella.1@gmail.com>
2022-03-31 16:18:16 +0200

    jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,3 +2,18 @@
 // Crea un array vuoto.
 // Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari inseriscilo nell’array.
+
+
+let div = document.querySelector('.app');
+let emptyArray = [];
+let userNumber='';
+let index = 0;
+while(index <= 5){
+    userNumber = prompt('inserisci un numero')
+    console.log(userNumber);
+    index++
+    if(userNumber % 2 !== 0){
+        emptyArray.push(userNumber)
+    } 
+}
+div.innerHTML += `<h1>Numeri dispari:${emptyArray}</h1>`;

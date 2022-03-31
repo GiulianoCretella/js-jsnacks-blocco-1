81841226ff7cba24a11df0bb6cbc54fcfe662600
GiulianoCretella <giulianocretella.1@gmail.com>
2022-03-31 17:27:14 +0200

    jsnack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,17 @@
 // JSnack 3
 // Chiedi un numero di 4 cifre all’utente
 // e calcola la somma di tutte le cifre che compongono il numero.
+let div = document.querySelector('.app');
+let userNumber = prompt('Inserisci un numero di 4 cifre');
+console.log(userNumber);
+
+let userArray = userNumber.split("");
+console.log(userArray);
+
+let somma = 0;
+let risultato= '';
+for(let i = 0; i < userArray.length; i++){
+     somma += parseInt(userArray[i])
+     console.log(somma);
+}
+div.append(somma);
